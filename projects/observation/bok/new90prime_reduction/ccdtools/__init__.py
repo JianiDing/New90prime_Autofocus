8793c95bd7fbb83b7eff8ed36e7e6bf35c7136b1
JianiDing <31973318+JianiDing@users.noreply.github.com>
2026-05-22 16:57:33 -0700

    Remove non-autofocus files from repo

--- projects/observation/bok/new90prime_reduction/ccdtools/__init__.py
+++
@@ -1,6 +0,0 @@
-"""ccdtools package
-
-Provides utilities and bad-pixel masking tools for BOK data reduction.
-"""
-
-__all__ = ["utilities", "bad_pixel_mask"]
--- projects/observation/bok/new90prime_reduction/ccdtools/__main__.py
+++
@@ -1,6 +0,0 @@
-"""Run ccdtools as a module. This delegates to the CLI by default."""
-from . import cli
-
-
-if __name__ == "__main__":
-    cli.main()
